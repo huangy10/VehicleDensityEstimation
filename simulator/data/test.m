d0255b589f790e3aeafd8bfcd345e0c73e99044e
Woody Huang <woody1573@163.com>
2015-11-17 21:27:38 +0800

    备份，准备开始修正fig2,3,4

--- simulator/data/test.m
+++ simulator/data/test.m
@@ -1,46 +1,8 @@
+clear;
 close all;
-clc
-clear
-% load 'data.mat'
-% figure()
-% plot(probe_loss_set, probe_rel_err, '-r')
-% hold on
-% plot(distance_loss_set, distance_rel_err, '-b')
-D = 9;
-m = 25;
-R = 100;
-lambda = 0.1;
-N = 2018;
-tau1 = 10:40;
-tau2 = tau1 + 5;
-P = 0:0.1:0.9;
+clc;
 
-phi1_1 = (P' * ((tau1 - D) .* exp(-lambda * (tau1 - D))) + (1-P)' * ((tau1 - D-m) .* exp(-lambda * (tau1 - m - D)))) * lambda ^ 2;
-phi1_2 = (1 - P)' * lambda^2 * ((tau1 - D - m).*exp(-lambda * (tau1 - D -m)));
-
-phi2_1 = (P' * ((tau2 - D) .* exp(-lambda * (tau2 - D))) + (1-P)' * ((tau2 - D-m) .* exp(-lambda * (tau2 - m - D)))) * lambda ^ 2;
-phi2_2 = (1 - P)' * lambda^2 * ((tau2 - D - m).*exp(-lambda * (tau2 - D -m)));
-
-PF1 = lambda * P' * ((tau1 - D + 1/lambda) .* exp(-lambda * (tau1 - D))) + lambda * (1-P)' * ((tau1 - D - m + 1/lambda) .* exp(-lambda * (tau1 - m - D)));
-PF2 = lambda * P' * ((tau2 - D + 1/lambda) .* exp(-lambda * (tau2 - D))) + lambda * (1-P)' * ((tau2 - D - m + 1/lambda) .* exp(-lambda * (tau2 - m - D)));
-
-
-coeff1 = N ./ (1 - PF1) ./ PF1;
-coeff2 = N ./ (1 - PF2) ./ PF2;
-
-J_11 = coeff1 .* phi1_1 .* phi1_1 + coeff2 .* phi2_1 .* phi2_1;
-J_22 = coeff1 .* phi1_2 .* phi1_2 + coeff2 .* phi2_2 .* phi2_2;
-J_21 = coeff1 .* phi1_2 .* phi1_1 + coeff2 .* phi2_2 .* phi2_1;
-J_12 = J_21;
-
-[p_num, tau_num] = size(J_11);
-
-CRB = ones(p_num, tau_num);
-
-for i = 1: p_num
-    for j = 1:tau_num
-        J = [J_11(i, j), J_12(i, j); J_21(i, j), J_22(i, j)];
-        tmp = inv(J);
-        CRB(i, j) = tmp(1, 1);
-    end
-end
+load 'data.mat';
+plot(distance_loss_set, distance_rel_err);
+hold on;
+plot(probe_loss_set, probe_rel_err);
--- simulator/tau.py
+++ simulator/tau.py
@@ -4,7 +4,7 @@ from matplotlib.pylab import *
 import math
 
 D = 9
-m = 5
+m = 25
 lmda = 0.1
 N = 2018
 
@@ -12,10 +12,11 @@ N = 2018
 def F(x):
     """ 累积分布函数
     """
-    if x < D:
-        return 0
-    else:
-        return 1-lmda * (x - D + 1/lmda) * math.exp(-lmda*(x-D))
+    return 1-lmda * (x - D + 1/lmda) * math.exp(-lmda*(x-D))
+    # if x < D:
+    #     return 0
+    # else:
+    #     return 1-lmda * (x - D + 1/lmda) * math.exp(-lmda*(x-D))
 
 
 def P_F_tau(tau, P=1):
@@ -24,27 +25,33 @@ def P_F_tau(tau, P=1):
 
 def P_F_dD(tau, P=1):
     """ 上述P_F_tau对D的导数
+     这里先去掉了定义域的限制
     """
-    if tau < D:
-        return 0
-    if tau - m < D:
-        return P * lmda * lmda * (tau - D) * math.exp(-lmda * (tau - D))
-    else:
-        return P * lmda * lmda * (tau - D) * math.exp(-lmda * (tau - D)) + \
-               (1 - P) * lmda * lmda * (tau - D - m) * math.exp(-lmda * (tau - D - m))
+    return P * lmda * lmda * (tau - D) * math.exp(-lmda * (tau - D)) + \
+            (1 - P) * lmda * lmda * (tau - D - m) * math.exp(-lmda * (tau - D - m))
+    # if tau < D:
+    #     return 0
+    # if tau - m < D:
+    #     return P * lmda * lmda * (tau - D) * math.exp(-lmda * (tau - D))
+    # else:
+    #     return P * lmda * lmda * (tau - D) * math.exp(-lmda * (tau - D)) + \
+    #            (1 - P) * lmda * lmda * (tau - D - m) * math.exp(-lmda * (tau - D - m))
 
 
 def P_F_dm(tau, P=1):
-    if tau < D:
-        return 0
-    if tau - m < D:
-        return 0
-    else:
-        return (1 - P) * lmda * lmda * (tau - D - m) * math.exp(-lmda * (tau - D - m))
+    """ 先去掉了定义域限制
+    """
+    return (1 - P) * lmda * lmda * (tau - D - m) * math.exp(-lmda * (tau - D - m))
+    # if tau < D:
+    #     return 0
+    # if tau - m < D:
+    #     return 0
+    # else:
+    #     return (1 - P) * lmda * lmda * (tau - D - m) * math.exp(-lmda * (tau - D - m))
 
 
 def main():
-    P_set = np.linspace(0.1, 1, 10)
+    P_set = np.linspace(0.1, 0.9, 10)
 
     tau1_set = np.arange(10, 40)
     result = np.zeros(shape=(len(P_set), len(tau1_set)))
@@ -55,9 +62,9 @@ def main():
             phi2 = np.matrix([P_F_dD(tau2, P), P_F_dm(tau2, P)])
             J = N / (1-P_F_tau(tau1, P)) / P_F_tau(tau1, P) * phi1.transpose() * phi1 + \
                 N / (1-P_F_tau(tau2, P)) / P_F_tau(tau2, P) * phi2.transpose() * phi2
+            print J
             result[i, j] = np.linalg.inv(J)[0, 0]
     return result
-
 
 
 if __name__ == "__main__":
